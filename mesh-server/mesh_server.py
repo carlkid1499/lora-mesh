f8c0680312c15758ad5bca18509365ba42a5f878
Carlos <port3116@vandals.uidaho.edu>
2021-01-28 16:19:40 -0800

    Cleanup
    -- cleanup code and adding methods where it makes sense

--- mesh-server/mesh_server.py
+++ mesh-server/mesh_server.py
@@ -11,6 +11,7 @@ import argparse
 import json
 import re
 import os
+import urllib.request
 import plotly.offline as py
 import plotly.graph_objects as go
 import networkx as nx
@@ -35,6 +36,69 @@ class MyServer(http.server.SimpleHTTPRequestHandler):
                           text=([text]),
                           mode='lines')
 
+    def get_webpage(self):
+        # Request the age http://192.168.4.1/logfile.txt
+        response = urllib.request.urlopen("http://192.168.4.1/logfile.txt")
+        # Decode the content and convert it to a string
+        content = response.read().decode("UTF-8")
+        return content
+
+    def parse_webpage_content(self, content):
+        # Here is our testdata. We need to read it in from a file and parse it
+        # This line here will match our JSON strings in the form of "node: {"3": [{"n":2,"r":0},{"n":2,"r":-24},{"n":255,"r":0}]}"
+        # It will find all occurrences and put them into a list.
+        data = [re.findall(r'^node: ({\"[0-9]+\": .*)', content)]
+        data_list_dict = []
+        for item in data:
+            # Convert each JSON string into a dict, but check for emtpy items
+            if item != []:
+                json_str = ""
+                json_str = json_str.join(item)
+                data_list_dict.append(json.loads(json_str))
+        
+        return data_list_dict
+    
+    def find_all_nodes(self, list_dict_to_search):
+        # Here is what we know:
+        # {"2": [{"n":1,"r":-44},{"n":255,"r":0},{"n":3,"r":-13}]}
+        # In the string above node 2 has valid connections to
+        # node 1 @ -44 dB and node 3 @ -13 db
+
+        # First let's find all the unique node names (keys) in the data list of dictionaries
+        # Now we try to create a nx graph
+        net = nx.Graph()
+
+        unique_node_list = []
+        for item in list_dict_to_search:
+            for key, value in item.items():
+                if key in unique_node_list:
+                    continue
+                else:
+                    unique_node_list.append(key)
+                    net.add_node(str(key), size=2)
+        
+        return net, unique_node_list
+
+    def add_edges_to_net_graph(self, list_dict_to_search, nodes_list, network_graph):
+        for item in list_dict_to_search:
+            for nodeid in nodes_list:
+                if nodeid in item:
+                    for node in item[nodeid]:
+                        node_name = node["n"]
+                        node_sigstrength = node["r"]
+
+                        # If the connection has a dB
+                        if node_sigstrength != 0:
+                            # Check if its a valid edge.  Valid: 3 -> 1,  Invalid: 3 -> 0
+                            if node_name != 0:
+                                #print("node:", ukey, "connects to:", node_name)
+                                network_graph.add_edge(
+                                    str(nodeid), str(node_name), weight=2)
+        
+        return network_graph
+
+
+
     def do_update_content(self):
         # Make a node trace
         node_trace = go.Scatter(x=[],
@@ -47,53 +111,14 @@ class MyServer(http.server.SimpleHTTPRequestHandler):
                                 marker=dict(color=[],
                                             size=[],
                                             line=None))
-
-        # Here is our testdata. We need to read it in from a file and parse it
-        # This line here will match our JSON strings in the form of "node: {"3": [{"n":2,"r":0},{"n":2,"r":-24},{"n":255,"r":0}]}"
-        # It will find all occurrences and put them into a list.
-        testdata = [re.findall(r'^node: ({\"[0-9]+\": .*)', line) for line in open('logfile.txt')]
-        testdata_dict_list = []
-        for item in testdata:
-            # Convert each JSON string into a dict, but check for emtpy items
-            if item != []:
-                json_str = ""
-                json_str = json_str.join(item)
-                data = json.loads(json_str)
-                print(data)
-                testdata_dict_list.append(data)
-
-        # Here is what we know:
-        # {"2": [{"n":1,"r":-44},{"n":255,"r":0},{"n":3,"r":-13}]}
-        # In the string above node 2 has valid connections to
-        # node 1 @ -44 dB and node 3 @ -13 db
-
-        # First let's find all the unique node names (keys) in the test_dict_list
-        # Now we try to create a nx graph
-        mesh_net = nx.Graph()
-
-        unique_key_list = []
-        for item in testdata_dict_list:
-            for key, value in item.items():
-                if key in unique_key_list:
-                    continue
-                else:
-                    unique_key_list.append(key)
-                    mesh_net.add_node(str(key), size=2)
-
-        for item in testdata_dict_list:
-            for ukey in unique_key_list:
-                if ukey in item:
-                    for node in item[ukey]:
-                        node_name = node["n"]
-                        node_sigstrength = node["r"]
-
-                        # If the connection has a dB
-                        if node_sigstrength != 0:
-                            # Check if its a valid edge.  Valid: 3 -> 1,  Invalid: 3 -> 0
-                            if node_name != 0:
-                                #print("node:", ukey, "connects to:", node_name)
-                                mesh_net.add_edge(
-                                    str(ukey), str(node_name), weight=2)
+        # Grab the web page content
+        webpage_content = self.get_webpage()
+        # Parse the web page content
+        data_list_dict = self.parse_webpage_content(webpage_content)
+        # Get a network graph an unique node list
+        mesh_net, nodes_list = self.find_all_nodes(data_list_dict)
+        # Find all the edges in network graph
+        mesh_net = self.add_edges_to_net_graph(data_list_dict, nodes_list, mesh_net)
 
         # Get positions for the nodes in G
         pos_ = nx.spring_layout(mesh_net)
